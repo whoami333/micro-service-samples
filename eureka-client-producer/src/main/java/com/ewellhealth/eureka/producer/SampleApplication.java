b3315061d7d11f100f2dea4f58e54d613ea26817
shellwalker <sagittarius1212@gmail.com>
2018-08-17 14:47:33 +0800

    add default loadbalance service choose logic

--- eureka-client-producer/src/main/java/com/ewellhealth/eureka/producer/SampleApplication.java
+++ eureka-client-producer/src/main/java/com/ewellhealth/eureka/producer/SampleApplication.java
@@ -2,13 +2,13 @@ package com.ewellhealth.eureka.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class SampleApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(SampleApplication.class, args);
     }
 }
--- eureka-client-producer/src/main/java/com/ewellhealth/eureka/producer/controller/EurekaClientController.java
+++ eureka-client-producer/src/main/java/com/ewellhealth/eureka/producer/controller/EurekaClientController.java
@@ -3,8 +3,9 @@ package com.ewellhealth.eureka.producer.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,15 +14,19 @@ import java.util.List;
 public class EurekaClientController {
 
     @Autowired
-    private DiscoveryClient client;
+    private DiscoveryClient discoveryClient;
 
-    @RequestMapping(value = "/eureka", method = RequestMethod.GET)
-    public String eurekaInfo() {
-        List<ServiceInstance> services = client.getInstances("eureka-producer");
-        StringBuilder stringBuilder = new StringBuilder();
-        for (ServiceInstance service : services) {
-            stringBuilder.append(service.getUri() + "/" + service.getPort() + ",");
-        }
-        return "Hello spring:" + stringBuilder.toString();
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return discoveryClient.getInstances(applicationName);
+    }
+
+    @RequestMapping("/load-balance/{applicationName}")
+    public String getLoadBalanceUrl(@PathVariable String applicationName) {
+        return loadBalancerClient.choose(applicationName).getUri().toString();
     }
 }
