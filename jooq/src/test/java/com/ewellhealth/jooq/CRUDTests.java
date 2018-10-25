210817509983c220b101fd1f812a8a968efad9ac
shellwalker <sagittarius1212@gmail.com>
2018-10-25 15:50:15 +0800

    add unit test code

---
+++ jooq/src/test/java/com/ewellhealth/jooq/CRUDTests.java
@@ -0,0 +1,51 @@
+package com.ewellhealth.jooq;
+
+import com.ewellhealth.jooq.autogen.tables.Username;
+import com.ewellhealth.jooq.autogen.tables.records.UsernameRecord;
+import org.jooq.DSLContext;
+import org.jooq.Result;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CRUDTests {
+    @Autowired
+    DSLContext dslContext;
+    Username username = Username.USERNAME;
+
+    @Test
+    public void testInsertJooq() {
+        dslContext.insertInto(username)
+                .columns(username.ID, username.NAME, username.ADDRESS)
+                .values(3, "li", "wuhan")
+                .execute();
+    }
+
+    @Test
+    public void testInsertJooq2() {
+//        dslContext.insertInto(username, )
+        dslContext.executeInsert(
+                new UsernameRecord(4, "sun", "shanghai"));
+    }
+
+    @Test
+    public void testJooqRead() {
+        Result<UsernameRecord> result = dslContext.selectQuery(username).fetch();
+        System.out.println("=======================");
+        System.out.println("=======================");
+        System.out.println("=======================");
+        System.out.println("=======================");
+        for (UsernameRecord record : result) {
+            System.out.println(record.getId() + ", "
+                    + record.getName() + ", " + record.getAddress());
+        }
+        System.out.println("=======================");
+        System.out.println("=======================");
+        System.out.println("=======================");
+        System.out.println("=======================");
+    }
+}
--- jooq/src/test/java/com/ewellhealth/jooq/JooqApplicationTests.java
+++ jooq/src/test/java/com/ewellhealth/jooq/JooqApplicationTests.java
@@ -1,6 +1,5 @@
 package com.ewellhealth.jooq;
 
-import com.ewellhealth.jooq.autogen.tables.Username;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JooqApplicationTests {
-
     @Test
     public void contextLoads() {
     }
